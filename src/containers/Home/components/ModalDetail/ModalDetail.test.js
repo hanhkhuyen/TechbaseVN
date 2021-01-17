0a36b9993e0ce6ad7bf80c5af9a448d4aedb2315
Khuyen <baby21802006@gmail.com>
2021-01-17 21:04:20 +0700

    Second commit

--- src/containers/Home/components/ModalDetail/ModalDetail.test.js
+++ src/containers/Home/components/ModalDetail/ModalDetail.test.js
@@ -6,8 +6,9 @@ test('should render ModalDetail component', () => {
     const infoDetail = [{Cases: 1537, Country: "Viet Nam", Date: "2021-01-17T00:00:00Z", Status: "confirmed"}];
     const status = 'confirmed';
     const selectCountry = 'Viet Nam';
+    const onCancel = () => {visible = false}
     
-    const { getByText } = render(<ModalDetail visible={visible} status={status} selectCountry={selectCountry} infoDetail={infoDetail}/>);
+    const { getByText } = render(<ModalDetail visible={visible} status={status} selectCountry={selectCountry} infoDetail={infoDetail} onCancel={onCancel}/>);
 
     const title = getByText(/Number of people/i);
     expect(title).toBeDefined();
